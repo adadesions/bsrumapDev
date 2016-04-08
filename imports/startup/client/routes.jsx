2f3489cf8550948d717fffaea28c7aec595d69d2
augisticz <augisticz@gmail.com>
2016-04-08 16:18:06 +0700

    Login and register

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -5,6 +5,8 @@ import AppContainer from '../../layouts/AppContainer.jsx';
 
 import Map1 from '../../pages/Map1.jsx';
 import Map2 from '../../pages/Map2.jsx';
+import Login from '../../ui/components/Login.jsx';
+import Register from '../../ui/components/Register.jsx';
 
 FlowRouter.route('/', {
   name: 'root',
@@ -20,6 +22,24 @@ FlowRouter.route('/map2', {
   action(){
     mount(AppContainer, {
       content: <Map2 />
+    })
+  }
+})
+
+FlowRouter.route('/login', {
+  name: 'login',
+  action(){
+    mount(AppContainer, {
+      content: <Login />
+    })
+  }
+})
+
+FlowRouter.route('/register', {
+  name: 'register',
+  action(){
+    mount(AppContainer, {
+      content: <Register />
     })
   }
 })
---
+++ imports/ui/components/Register.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+
+const layoutLogin = {
+  width: '100%',
+  height: '100vh',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  background: '#092756',
+  background: '-moz-radial-gradient(0% 100%, ellipse cover, rgba(104,128,138,.4) 10%,rgba(138,114,76,0) 40%),-moz-linear-gradient(top,  rgba(57,173,219,.25) 0%, rgba(42,60,87,.4) 100%), -moz-linear-gradient(-45deg,  #670d10 0%, #092756 100%)',
+	background: '-webkit-radial-gradient(0% 100%, ellipse cover, rgba(104,128,138,.4) 10%,rgba(138,114,76,0) 40%), -webkit-linear-gradient(top,  rgba(57,173,219,.25) 0%,rgba(42,60,87,.4) 100%), -webkit-linear-gradient(-45deg,  #670d10 0%,#092756 100%)',
+	background: '-o-radial-gradient(0% 100%, ellipse cover, rgba(104,128,138,.4) 10%,rgba(138,114,76,0) 40%), -o-linear-gradient(top,  rgba(57,173,219,.25) 0%,rgba(42,60,87,.4) 100%), -o-linear-gradient(-45deg,  #670d10 0%,#092756 100%)',
+	background: '-ms-radial-gradient(0% 100%, ellipse cover, rgba(104,128,138,.4) 10%,rgba(138,114,76,0) 40%), -ms-linear-gradient(top,  rgba(57,173,219,.25) 0%,rgba(42,60,87,.4) 100%), -ms-linear-gradient(-45deg,  #670d10 0%,#092756 100%)',
+	background: '-webkit-radial-gradient(0% 100%, ellipse cover, rgba(104,128,138,.4) 10%,rgba(138,114,76,0) 40%), linear-gradient(to bottom,  rgba(57,173,219,.25) 0%,rgba(42,60,87,.4) 100%), linear-gradient(135deg,  #670d10 0%,#092756 100%)',
+};
+const blogLogin = {
+  width: '70%',
+  height: '60%',
+  textAlign: 'center',
+};
+const boxInput = {
+  paddingLeft: '3em',
+  paddingRight: '3em',
+};
+const textLogin = {
+  color: '#fff',
+  textShadow: '0 0 10px rgba(0,0,0,0.3)',
+  letterSpacing: '1px',
+};
+export default class Register extends React.Component {
+  clickBack(){
+    FlowRouter.go('login');
+  }
+  render(){
+    return(
+      <div style={layoutLogin} className="login-layout">
+        <div style={blogLogin}>
+          <h1 style={textLogin}>Register</h1><br/>
+            <input id="username" type="text" placeholder="Username" className="box-input"/>
+            <input id="password" type="password" placeholder="Password" className="box-input"/>
+            <input id="email" type="email" placeholder="Email" className="box-input"/>
+            <a className="waves-effect waves-light btn-large"><h4>Submit</h4></a>
+            <a onClick={this.clickBack} className="waves-effect waves-light btn-large"><h4>Back</h4></a>
+        </div>
+      </div>
+    )
+  }
+}
