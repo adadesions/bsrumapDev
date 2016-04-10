35ec2f0fc9f6a5f6149c6ba0ffa0ab94ecfb9866
augisticz <augisticz@gmail.com>
2016-04-10 12:33:53 +0700

    Edit Responsive login

--- imports/pages/Login.jsx
+++ imports/pages/Login.jsx
@@ -15,9 +15,12 @@ const layoutLogin = {
 	background: '-webkit-radial-gradient(0% 100%, ellipse cover, rgba(104,128,138,.4) 10%,rgba(138,114,76,0) 40%), linear-gradient(to bottom,  rgba(57,173,219,.25) 0%,rgba(42,60,87,.4) 100%), linear-gradient(135deg,  #670d10 0%,#092756 100%)',
 };
 const blogLogin = {
-  width: '70%',
-  height: '60%',
+  width: '80%',
+  height: '100%',
   textAlign: 'center',
+  display: 'flex',
+  justifyContent: 'center',
+  flexFlow: 'column',
 };
 const boxInput = {
   paddingLeft: '3em',
